a9ef1794cf37632330cf967452a8735b4ea9d09a
BMS <jbongmm@gmail.com>
2021-07-23 12:25:14 +0900

    0723 commit 3

--- gis_001/settings.py
+++ gis_001/settings.py
@@ -55,7 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp',
-    'bootstrap4'
+    'bootstrap4',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
