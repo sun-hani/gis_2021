a6b5fe3cb42fee55a5aeb2b1303d59dbb56d3265
BMS <jbongmm@gmail.com>
2021-07-27 11:24:36 +0900

    0727 commit 1

--- profileapp/models.py
+++ profileapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='profile')
 
