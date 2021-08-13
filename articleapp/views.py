97004bd5a8772f8da3fc02d4be2853d279b71b94
BMS <jbongmm@gmail.com>
2021-08-13 11:28:59 +0900

    0813 commit 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -57,4 +57,4 @@ class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
-    paginate_by = 1 #한페이에 보이는 갯수#
+    paginate_by = 20 #한페이에 보이는 갯수#
